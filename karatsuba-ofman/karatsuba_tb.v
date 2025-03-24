2b6d64b2ee48fb8a9f3aba01f14aa62f44f7ab85
Ahmed Alharbi <48549687+akaalharbi@users.noreply.github.com>
2025-03-24 02:46:59 +0100

    Rewrote a simple implementation of Karatsuba without overflow by using substraction

--- karatsuba-ofman/karatsuba_tb.v
+++ karatsuba-ofman/karatsuba_tb.v
@@ -1,68 +1,24 @@
-// This is the karat_mul module with recursion for project 
-// Karatsuba_multiplier_HDL
-// Copyright (C) 2020 JC-S
-// 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-//-----------------------------------------------------------------------------
-//
-//FILE NAME     : karatsuba_tv.v
-//AUTHOR        : JC-S
-//FUNCTION      : Main mult module w. recursion for project 
-//                Karatsuba_multiplier_HDL
-//INITIAL DATE  : 2020/06/29
-//VERSION       : 1.1
-//CHANGE LOG    : 1.0: initial version.
-//
-//CHANGE LOG    : 1.1: Use wireal and in the recursion
-//-----------------------------------------------------------------------------
 
 
 module top_module ();
-   localparam N = 16;
-   localparam log2_N = 4;
+   localparam N = 8;
    // bit index number 0
    reg [N-1:0] a;
    reg [N-1:0] b;
    reg [2*N - 1:0] ab;
    
-   initial begin 
-      $display("N = %0d, log2(N) = %0d", N, log2_N);
-   end
    
-   /*
-   stage #(.N(N),
-           .log2_N(log2_N),
-           .stage_number(2))
-   M
-     (.inputs(a), 
-      .outputs(Ma));
-     */ 
+   initial begin 
+      $display("N = %0d", N);
+   end
 
    // Instantiate expmob1 and call the instance M
-    karat_mult_recursion #(.wI(N),
-			   .wO(2*N),
-			   .nSTAGE(log2_N))
-   AB
-     (.iX(a), 
-      .iY(Ma),
-      .oO(ab));
-	
+   karatsuba #(.N(N)) mul (.A(a), .B(b), .C(ab));
+   
    initial begin // 
-      a = 'b1100_0000_0000_0000;
-      b = 'b1111_0000_0010_1010;
+      a = 'b1100100;
+      b = 'b1001011;
+      
       // delay of 100 time units
       //#100 a = 'b1111_0000_0000_0000;
       //#100 a = 'b0000_0000_0000_0000;
@@ -79,140 +35,85 @@ module top_module ();
 
 endmodule
 
+// Compute C := A*B
+module karatsuba
+  #(parameter N=16) // N has to be a power of 2
+   ( input wire [N-1:0]	   A,
+     input wire [N-1:0]	   B,
+     output wire [2*N-1:0] C);
 
 
-
-module karat_mult_recursion #(parameter wI = 1024,   // Length of the operand iX and iY
-			      parameter wO = 2 * wI, // Length of the output 
-			      parameter nSTAGE = 10 // log2(wI)
-			      )
-   (// data IOs
-    input wire [wI-1:0]  iX,
-    input wire [wI-1:0]  iY,
-    output wire [wO-1:0] oO,
-    // control IOs
-    input wire	 clk,
-    input wire	 rst_n,
-    input wire	 i_enable,
-    output wire o_finish
-    );
-
-   localparam wI_pt = wI / 2;
-
-   wire	      finish_p, finish_q, finish_ts;
-
-   wire [wI_pt-1:0] X_hi, X_lo;
-   wire [wI_pt-1:0] Y_hi, Y_lo;
-
-   assign  {X_hi, X_lo} = iX;
-   assign  {Y_hi, Y_lo} = iY;
-
-   wire   [wI_pt*2-1:0]   p;
-   wire [wI_pt*2-1:0]	  q;
-   wire [wI_pt:0]	  r;
-   wire [wI_pt:0]	  s;
-
-   assign  r = X_hi + X_lo;
-   assign  s = Y_hi + Y_lo;
-
-   wire   [wI_pt*2:0]     u;
-   assign  u = p + q;
-
-   wire   [wI+1:0]        t;
-   wire			  r_hi, s_hi;
-   wire [wI_pt-1:0]	  r_lo, s_lo;
-
-   assign  {r_hi, r_lo} = r;
-   assign  {s_hi, s_lo} = s;
-
-   wire   [wI-1:0]        t_s;
-
+   // compute karatsuba recursively	
    generate
-      begin: p_eq_Xhi_x_Yhi
-	 if(nSTAGE == 1)
-	   begin: p_direct_mult
-              assign  p = X_hi & Y_hi;
-              assign  finish_p = i_enable;
-	   end
-	 else
-	   begin: p_karat_mult
-              karat_mult_recursion #(.wI         (wI_pt),
-				     .nSTAGE     (nSTAGE-1))
-              u_karat_mult_recursion_p (.iX         (X_hi),
-					.iY         (Y_hi),
-					.oO         (p),
-					.clk        (clk),
-					.rst_n      (rst_n),
-					.i_enable   (i_enable),
-					.o_finish   (finish_p)
-					);
-	   end
-      end
+      if (N == 1)  // base case
+	begin
+	   // The output is just the and of the two bits
+	   assign C = A&B;
+	end
+
+      else // recurse with all the complications 
+	begin
+	   localparam N_half = N>>1;
+	   
+	   wire [ (N/2) - 1 : 0] A_l;
+	   wire [ (N/2) - 1 : 0] A_h;
+	   wire [ (N/2) - 1 : 0] B_l;
+	   wire [ (N/2) - 1 : 0] B_h;
+	   
+	   assign A_l = A[N/2 - 1 : 0];
+	   assign A_h = A[ N  - 1 : N/2];
+	   assign B_l = B[N/2 - 1 : 0];
+	   assign B_h = B[ N  - 1 : N/2];
+	   
+	   // N/2 + 1 bits for the difference, the extra bit is for the sign
+       wire [ (N/2) : 0] A_m; // := A_l - A_h
+       wire [ (N/2) : 0] B_m; // := B_h - B_l
+       wire [ (N/2) : 0] abs_A_m; // := |A_l - A_h|
+       wire [ (N/2) : 0] abs_B_m; // := |B_h - B_l|
+	   assign A_m = A_l - A_h;
+	   assign B_m = B_h - B_l;
+	   
+        
+	   wire  sign_A_m;
+	   wire  sign_B_m;
+	   wire  sign;   
+       assign sign_A_m = A_m[N/2];
+       assign sign_B_m = B_m[N/2];
+       assign abs_A_m = (1 - 2*sign_A_m)*A_m;
+       assign abs_B_m = (1 - 2*sign_B_m)*B_m;
+
+        // 1 - 2*sign = -1 if  (A_m*B_m) < 0, otherwise 1
+	   assign sign = sign_A_m ^ sign_B_m;
+
+	   // P3 :=  A_h * B_h
+	   wire [N-1:0] P3; // :=  A_h * B_h
+	   karatsuba #(.N(N/2)) Ah_Bh (.A(A_h), .B(B_h), .C(P3));
+	   
+	   wire [N-1:0]	P2; // :=  A_l*B_l
+	   karatsuba #(.N(N/2)) Al_Bl (.A(A_l), .B(B_l), .C(P2));
+	   
+	   // (A_l - A_h)(B_h - B_l)  = A_m[N/2-1:0] * B_m[N/2-1:0]
+	   //  without the sign bit
+	   wire [N-1:0]	P1;
+	   karatsuba #(.N(N/2)) Am_Bm (.A(abs_A_m[N/2 - 1 : 0]), 
+                                   .B(abs_B_m[N/2 - 1 : 0]),
+                                   .C(P1));
+
+
+	always @(*) begin 
+        if (^C !== 1'bx) begin
+            $display("Recurse: N=%0d | A=%d, B=%d | A_l=0b%b, A_h=0b%b | B_l=0b%b , B_h=0b%b | A_m=0b%b , B_m=0b%b , sign=%b| P1=%0d , P2=%0d , P3=%d | C = %0d", 
+                    N, A, B, A_l, A_h, B_l, B_h, A_m, B_m, sign, P1, P2, P3, (1<<N) * P3 + (1 << (N/2))*(P3 + P2 + (1-2*sign)*P1) + P2);
+       	end // if
+    end // always
+	   // (A_l - A_h)(B_h - B_l) + A_h*B_h + A_l*B_l
+	   // (1 - 2*sign) = -1 if P1 shoulde be < 0
+	   assign C = (1<<N) * P3 
+		    + (1 << (N/2))*(P3 + P2 + (1-2*sign)*P1)
+		    + P2;
+	   
+	end
    endgenerate
+endmodule // karatsuba
 
-   generate
-      begin: q_eq_Xlo_x_Ylo
-	 if(nSTAGE == 1)
-	   begin: q_direct_mult
-              assign  q = X_lo & Y_lo;
-              assign  finish_q = i_enable;
-	   end
-	 else
-	   begin: q_karat_mult
-              karat_mult_recursion #(
-				     .wI         (wI_pt),
-				     .nSTAGE     (nSTAGE-1)
-				     )
-              u_karat_mult_recursion_q (
-					.iX         (X_lo),
-					.iY         (Y_lo),
-					.oO         (q),
-					.clk        (clk),
-					.rst_n      (rst_n),
-					.i_enable   (i_enable),
-					.o_finish   (finish_q)
-					);
-	   end
-      end
-   endgenerate
 
-   generate
-      begin: ts_eq_rlo_x_slo
-	 if(nSTAGE == 1)
-	   begin: ts_direct_mult
-              assign  t_s = r_lo & s_lo;
-              assign  finish_ts = i_enable;
-	   end
-	 else
-	   begin: ts_karat_mult
-              karat_mult_recursion #(
-				     .wI         (wI_pt),
-				     .nSTAGE     (nSTAGE-1)
-				     )
-              u_karat_mult_recursion_ts (
-					 .iX         (r_lo),
-					 .iY         (s_lo),
-					 .oO         (t_s),
-					 .clk        (clk),
-					 .rst_n      (rst_n),
-					 .i_enable   (i_enable),
-					 .o_finish   (finish_ts)
-					 );
-	   end
-      end
-   endgenerate
-
-   assign t = ((r_hi & s_hi) << wI) + ((r_hi * s_lo + s_hi * r_lo) << wI_pt) + t_s;
-
-   assign oO = (p << wI) + ((t - u) << wI_pt) + q;
-
-   /* -----\/----- EXCLUDED -----\/-----
-    // Vivado complains about 
-    always  @(posedge clk or negedge rst_n)
-    if(!rst_n)
-    o_finish    <= 1'b0;
-    else
-    o_finish    <= finish_p && finish_q && finish_ts;
-
-    -----/\----- EXCLUDED -----/\----- */
-endmodule
